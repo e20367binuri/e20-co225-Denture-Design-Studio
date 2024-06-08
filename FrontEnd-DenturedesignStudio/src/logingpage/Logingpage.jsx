ff8056f6b89a7e98ee401e7d7ad3eeacc25bd0ac
ravindu439 <e20439@eng.pdn.ac.lk>
2024-06-09 02:48:19 +0530

    changes1

--- FrontEnd-DenturedesignStudio/src/logingpage/Logingpage.jsx
+++ FrontEnd-DenturedesignStudio/src/logingpage/Logingpage.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
-import "./Logingpage.css"
+import React, { useState } from 'react';
+import "./Logingpage.css";
 import Back from 'E:/e20-co225-Denture-Design-Studio/FrontEnd-DenturedesignStudio/src/backbutton/Back.jsx';
 import { useNavigate } from 'react-router-dom';
+
 function Loginpage() {
-  let navigale =useNavigate();
-function clickhandle(path) {
-  navigale(path);
-}
+  let navigate = useNavigate();
+
+  function clickhandle(path) {
+    navigate(path);
+  }
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    clickhandle("/");
+  };
+
   return (
     <div className="logingpage">
-      <div className="back"><Back  onclick={()=>clickhandle("/")}></Back></div>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Salsa&display=swap" />
-      <div className='contentbox'>
-        
-        <h2 className='header'>Log in to your account</h2>
-        <div className="userinput" id='input1'>
-          <h3 className='inputs'>Username:</h3>
-          <input type="text" />
-        </div>
-        <div className="userinput" id='input2'>
-          <h3 className='inputs'>Password:</h3>
-          <input type="password" />
-          <p id ='forget'><link rel="#" href="" />Forgot Password?</p>
-        </div>
-        <div>
-          <button className="login2" onClick={()=>clickhandle("/")}>Login</button>
-        </div>
+      <div className="back">
+        <Back onclick={() => clickhandle("/")}></Back>
       </div>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Salsa&display=swap" />
+      <form onSubmit={handleSubmit}>
+        <div className='contentbox'>
+          <h2 className='header'>Log in to your account</h2>
+          <div className="userinput" id='input1'>
+            <h3 className='inputs'>Username:</h3>
+            <input type="text" onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div className="userinput" id='input2'>
+            <h3 className='inputs'>Password:</h3>
+            <input type="password" onChange={(e) => setPassword(e.target.value)} />
+            <p id='forget'><link rel="#" href="" />Forgot Password?</p>
+          </div>
+          <div>
+            <button type='submit' className="login2">Login</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
